41eeeedd600731f130c27ff4568e59eb6fb2390c
Landysh Khusaenova <landysh.kh@gmail.com>
2026-02-22 14:05:26 +0100

    Add docstrings and define the main function

--- filter_users.py
+++ filter_users.py
@@ -2,25 +2,29 @@ import json
 
 
 def load_file():
+    """ Load the users file and return it as a dictionary """
+
     with open("users.json", "r") as file:
         users = json.load(file)
         return users
 
 
 def filter_users_by_key(key, value):
+    """ Filter users by a given key and value """
+
     users = load_file()
 
     for user in users:
         if isinstance(user[f"{key}"], str):
-            user[f"{key}"] = user[f"{key}"].lower()
+            user[key] = user[f"{key}"].lower()
 
-    filtered_users = [user for user in users if user[f"{key}"] == value]
+    filtered_users = [user for user in users if user[key] == value]
 
     for user in filtered_users:
         print(user)
 
 
-if __name__ == "__main__":
+def main():
     filter_option = input("What would you like to filter by? ('name', 'age' or 'email'): ").strip().lower()
 
     if filter_option == "name":
@@ -30,7 +34,11 @@ if __name__ == "__main__":
         age_to_search = int(input("Enter an age to filter users: ").strip())
         filter_users_by_key("age", age_to_search)
     elif filter_option == "email":
-        email_to_search = input("Enter an email to filter users: ").strip()
+        email_to_search = input("Enter an email to filter users: ").lower().strip()
         filter_users_by_key("email", email_to_search)
     else:
         print("Filtering by that option is not yet supported.")
+
+
+if __name__ == "__main__":
+    main()
