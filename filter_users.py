2348da71170677e22af6e0387268d03f3d6e0e8c
Landysh Khusaenova <landysh.kh@gmail.com>
2026-02-22 13:51:37 +0100

    Add new function for file donload, modify function filter_users_by_name to filter_users_by_key

--- filter_users.py
+++ filter_users.py
@@ -1,21 +1,33 @@
 import json
 
 
-def filter_users_by_name(name):
+def load_file():
     with open("users.json", "r") as file:
         users = json.load(file)
+        return users
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+
+def filter_users_by_key(key, value):
+    users = load_file()
+
+    for user in users:
+        if isinstance(user[f"{key}"], str):
+            user[f"{key}"] = user[f"{key}"].lower()
+
+    filtered_users = [user for user in users if user[f"{key}"] == value]
 
     for user in filtered_users:
         print(user)
 
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? ('name' or 'age'): ").strip().lower()
 
     if filter_option == "name":
-        name_to_search = input("Enter a name to filter users: ").strip()
-        filter_users_by_name(name_to_search)
+        name_to_search = input("Enter a name to filter users: ").lower().strip()
+        filter_users_by_key("name", name_to_search)
+    elif filter_option == "age":
+        age_to_search = int(input("Enter an age to filter users: ").strip())
+        filter_users_by_key("age", age_to_search)
     else:
         print("Filtering by that option is not yet supported.")
